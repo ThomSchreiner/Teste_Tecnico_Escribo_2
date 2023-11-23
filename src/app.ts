fabcf92a69e57eb8b6f10b91b6da26414e3c1570
ThomSchreiner <iteano.thom@hotmail.com>
2023-11-23 09:33:27 -0300

    feat: add global handle error

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
+import "express-async-errors";
 import express from "express";
+import { handleError } from "./errors";
+import { userRoutes } from "./routes/user.routes";
 
 export const app = express();
 
 app.use(express.json());
+
+app.use("/users", userRoutes);
+
+app.use(handleError);
