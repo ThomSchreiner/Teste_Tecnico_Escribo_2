e265c65edee33c1c37d9a5458696b34bfa5adb52
ThomSchreiner <iteano.thom@hotmail.com>
2023-11-23 17:30:40 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import express from "express";
 import { handleError } from "./errors";
 import { userRoutes } from "./routes/user.routes";
 import { loginRoutes } from "./routes/login.routes";
+import cors from "cors";
 
 export const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/login", loginRoutes);
